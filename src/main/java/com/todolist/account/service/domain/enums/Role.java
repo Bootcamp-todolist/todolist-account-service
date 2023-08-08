29cbac9a7092176ecbb230db59e974a217d2f1f1
kexin.an <kexin.an@thoughtworks.com>
2023-08-08 21:59:05 +0800

    [N/A] feat: add AdminAccount domain

---
+++ src/main/java/com/todolist/account/service/domain/enums/Role.java
@@ -0,0 +1,6 @@
+package com.todolist.account.service.domain.enums;
+
+public enum Role {
+  ADMIN,
+  USER
+}
---
+++ src/main/java/com/todolist/account/service/domain/models/UserAccount.java
@@ -0,0 +1,21 @@
+package com.todolist.account.service.domain.models;
+
+import com.todolist.account.service.domain.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class UserAccount {
+
+  protected String id;
+  protected Role role;
+  protected String username;
+  protected String password;
+}
