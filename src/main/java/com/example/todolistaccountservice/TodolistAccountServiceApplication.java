ba7db4d73868b424e6dc2e09e3508d4328ad86b6
kexin.an <kexin.an@thoughtworks.com>
2023-08-08 20:48:36 +0800

    init

---
+++ src/main/java/com/example/todolistaccountservice/TodolistAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolistaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistAccountServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodolistAccountServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/todolistaccountservice/TodolistAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolistaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistAccountServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
