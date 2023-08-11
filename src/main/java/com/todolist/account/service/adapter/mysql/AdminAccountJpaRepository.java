22903ceb3a15a91e3a3db1908b6025a620b8195b
kexin.an <kexin.an@thoughtworks.com>
2023-08-11 10:26:53 +0800

    [9] chore: format code

--- src/main/java/com/todolist/account/service/adapter/mysql/AdminAccountJpaRepository.java
+++ src/main/java/com/todolist/account/service/adapter/mysql/AdminAccountJpaRepository.java
@@ -3,7 +3,7 @@ package com.todolist.account.service.adapter.mysql;
 import com.todolist.account.service.adapter.mysql.models.AdminAccountPersistModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdminAccountJpaRepository extends JpaRepository<AdminAccountPersistModel,String> {
+public interface AdminAccountJpaRepository extends JpaRepository<AdminAccountPersistModel, String> {
 
   AdminAccountPersistModel findByUsername(String username);
 }
--- src/main/java/com/todolist/account/service/adapter/mysql/AdminAccountRepositoryImpl.java
+++ src/main/java/com/todolist/account/service/adapter/mysql/AdminAccountRepositoryImpl.java
@@ -9,16 +9,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class AdminAccountRepositoryImpl implements AdminAccountRepository {
+
   private final AdminAccountJpaRepository adminAccountJpaRepository;
 
   @Override
   public AdminAccount findByUsername(String username) {
-    return AdminAccountPersistModelMapper.MAPPER.toDomain(adminAccountJpaRepository.findByUsername(username));
+    return AdminAccountPersistModelMapper.MAPPER.toDomain(
+        adminAccountJpaRepository.findByUsername(username));
   }
 
   @Override
   public void save(AdminAccount account) {
-
     adminAccountJpaRepository.save(AdminAccountPersistModelMapper.MAPPER.toPersistModel(
         account));
   }
